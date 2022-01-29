64ae58179af267998993c15717dfc07266c8e228
Yuvaan-Goel <78798914+Yuvaan-Goel@users.noreply.github.com>
2022-01-29 08:51:18 +0530

    Add files via upload

---
+++ innovation-showing-js.js
@@ -0,0 +1,16 @@
+function robot() {
+    window.location = "robot.html";
+    
+}
+
+function vax() {
+    window.location = "vax-html - Copy.html";
+}
+
+function kit() {
+    window.location = "kit-html.html"; 
+}
+
+function pills() {
+    window.location = "COVID pills-html.html"; 
+}
